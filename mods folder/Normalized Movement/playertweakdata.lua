85850f1499470ab1902744b1034d2ea01af7dd90
Sokolov49 <81686591+Sokolov49@users.noreply.github.com>
2025-11-09 20:57:13 +0100

    byebye

--- mods folder/Normalized Movement/playertweakdata.lua
+++ mods folder/Normalized Movement/playertweakdata.lua
@@ -1,5 +1,4 @@
 Hooks:PostHook(PlayerTweakData, "init", "gravity_init", function(self, tweak_data)
 	self.gravity = 1275
 	self.stances.default.standard.head.translation = Vector3(0, 0, 165) -- 165 for actual height
-	self.stances.default.crouched.head.translation = Vector3(0, 0, 95)
 end)
