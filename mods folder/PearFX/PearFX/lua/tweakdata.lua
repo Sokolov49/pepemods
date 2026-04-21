38a68f8870b407a0afd8253f52e37840d9d7d70c
Sokolov49 <81686591+Sokolov49@users.noreply.github.com>
2026-04-21 17:29:30 +0200

    pee fx

---
+++ mods folder/PearFX/PearFX/lua/tweakdata.lua
@@ -0,0 +1,12 @@
+if not tweak_data then return end
+tweak_data.projectiles.frag.effect_name = "effects/explosion_grenade"
+tweak_data.projectiles.frag_com.effect_name = "effects/explosion_grenade"
+tweak_data.projectiles.launcher_frag.effect_name = "effects/explosion_grenade_launcher"
+tweak_data.projectiles.projectiles.launcher_frag_m79.effect_name = "effects/explosion_grenade_launcher"
+tweak_data.projectiles.launcher_m203.effect_name = "effects/explosion_grenade_launcher"
+tweak_data.projectiles.launcher_frag_m32.effect_name = "effects/explosion_grenade_launcher"
+tweak_data.projectiles.launcher_incendiary_m32.effect_name = "effects/explosion_incendiary"
+tweak_data.projectiles.fir_com.effect_name = "effects/explosion_incendiary"
+-- tweak_data.projectiles.launcher_rocket
+tweak_data.projectiles.launcher_incendiary.effect_name = "effects/explosion_incendiary"
+-- projectiles.launcher_poison.poison_gas_effect = "effects/particles/explosions/poison_gas"
