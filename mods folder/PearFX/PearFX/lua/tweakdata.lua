b60e0a9c9298a649603b9abaa5f431a0d7e4f143
Sokolov49 <81686591+Sokolov49@users.noreply.github.com>
2026-05-19 18:23:54 +0200

    per

--- mods folder/PearFX/PearFX/lua/tweakdata.lua
+++ mods folder/PearFX/PearFX/lua/tweakdata.lua
@@ -9,3 +9,5 @@ tweak_data.projectiles.fir_com.effect_name = "effects/explosion_incendiary"
 -- tweak_data.projectiles.launcher_rocket
 tweak_data.projectiles.launcher_incendiary.effect_name = "effects/explosion_incendiary"
 -- projectiles.launcher_poison.poison_gas_effect = "effects/particles/explosions/poison_gas"
+-- tweak_data.projectiles.flun_flare.effect_name = "effects/particles/flaregun_explosion_sky"
+tweak_data.projectiles.flun_flare.impact_effect_name = "effects/particles/flaregun_explosion_sky"
